5e3cdcacd5c41007cde56e883ea39a6c7890134b
Dennis <dmolter@gmail.com>
2013-04-30 11:56:56 +0200

    initial commit

---
+++ SANDBOX/src/test/MainTester.java
@@ -0,0 +1,10 @@
+package test;
+
+import ui.MyFirstUI;
+
+public class MainTester {
+  public static void main(String[] args) {
+    MyFirstUI view = new MyFirstUI();
+    view.setVisible(true);
+  }
+} 
